90ea0ea605f375600406ce16143bc811031a8cad
Chao Chen <chaochen@live.com>
2024-09-19 19:19:45 -0400

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <>Boba Emotes</>;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

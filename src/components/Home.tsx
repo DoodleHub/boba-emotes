e7b4bf2d18b5b8dfbc7503641c1cf4f883c6f646
Chao Chen <chaochen@live.com>
2024-09-19 20:00:43 -0400

    chore: update home styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col gap-8 w-1/2 items-center">
-        <p className="font-bold">
+        <p className="text-lg font-medium">
           Introducing BobaEmotes: Express Yourself with Adorable Boba Stickers!
         </p>
         <p>
